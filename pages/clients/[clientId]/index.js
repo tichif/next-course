0e694f74e5cf2126107da4aefc4d4794989a4d83
Charles-Hebert Dalzon <charlby5@gmail.com>
2021-06-14 17:28:00 -0400

    Navigate programmatically

--- pages/clients/[clientId]/index.js
+++ pages/clients/[clientId]/index.js
@@ -1,7 +1,16 @@
+import { useRouter } from 'next/router';
+
 const ClientsProjectPage = () => {
+  const router = useRouter();
+
+  const buttonClickedHandler = () => {
+    router.push('/clients/tichif/123');
+  };
+
   return (
     <div>
       <h1>The Clients Projects Page</h1>
+      <button onClick={buttonClickedHandler}>Load Project</button>
     </div>
   );
 };
